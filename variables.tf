e78534fffcb9e675d9cd5614351368f4a4a71fe5
IlyaSchelokov <shelokov.ilya@yandex.ru>
2024-09-19 16:36:38 +0300

    description of variables

---
+++ variables.tf
@@ -0,0 +1,91 @@
+variable "my_oauth_token" {
+}
+
+variable "my_folder" {
+}
+
+variable "k8s_name" {
+}
+
+variable "k8s_net_name" {
+}
+
+variable "k8s_subnet_name" {
+}
+
+variable "k8s_zone_name" {
+}
+
+variable "k8s_sa_account" {
+}
+
+variable "k8s_name_kms_key" {
+}
+
+variable "k8s_name_sec_group" {
+}
+
+variable "node_group_name" {
+}
+
+variable "node_number_cores" {
+}
+
+variable "node_core_fraction" {
+}
+
+variable "node_size_ram" {
+}
+
+variable "node_size_ssd" {
+}
+
+variable "node_scale_size" {
+}
+
+variable "node_max_expansion" {
+}
+
+variable "node_max_unavailable" {
+}
+
+variable "node_lables_for_grouping_k8s" {
+}
+
+variable "node_label" {
+}
+
+variable "vm_name_sec_group" {
+}
+
+variable "vm_name" {
+}
+
+variable "vm_zone_name" {
+}
+
+variable "vm_number_cores" {
+}
+
+variable "vm_size_ram" {
+}
+
+variable "vm_core_fraction" {
+}
+
+variable "vm_image_id" {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
