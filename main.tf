ce3b9c154cf6e62c168f2c7d5634385a567aab29
IlyaSchelokov <91783798+IlyaSchelokov@users.noreply.github.com>
2024-09-25 09:18:58 +0300

    correct and recreated resourses for k8s and add block for Ansible

--- main.tf
+++ main.tf
@@ -1,104 +1,28 @@
 resource "yandex_resourcemanager_folder" "myfolder" {
   # Создание в облаке YC папки для размещения ресурсов.
-  cloud_id    = local.cloud_id
-  name        = var.my_folder
+  cloud_id    =  local.cloud_id
+  name        = "tomat"
   description = "myfolder"
-}
-
-resource "yandex_kubernetes_cluster" "k8s-zonal" {
-  # Создание кластера k8s.
-  name       = var.k8s_name
-  network_id = yandex_vpc_network.mynet.id
-  folder_id  = yandex_resourcemanager_folder.myfolder.id
-  master {
-    master_location {
-      zone      = yandex_vpc_subnet.mysubnet.zone
-      subnet_id = yandex_vpc_subnet.mysubnet.id
-    }
-    public_ip = true
-  }
-  service_account_id      = yandex_iam_service_account.myaccount.id
-  node_service_account_id = yandex_iam_service_account.myaccount.id
-  depends_on = [
-    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
-    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
-    yandex_resourcemanager_folder_iam_member.images-puller,
-    yandex_resourcemanager_folder_iam_member.encrypterDecrypter,
-    yandex_resourcemanager_folder_iam_member.load-balancer-admin
-  ]
-  kms_provider {
-    key_id = yandex_kms_symmetric_key.kms-key.id
-  }
 }
 
 resource "yandex_vpc_network" "mynet" {
   # Создание внутренней сети кластера и его компонентов.
-  name      = var.k8s_net_name
+  name      = "mynet"
   folder_id = yandex_resourcemanager_folder.myfolder.id
 }
 
 resource "yandex_vpc_subnet" "mysubnet" {
   # Создание внутренней подсети кластера и его компонентов.
-  name           = var.k8s_subnet_name
+  name           = "mysubnet"
   v4_cidr_blocks = ["192.168.10.0/27"]
-  zone           = var.k8s_zone_name
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.mynet.id
   folder_id      = yandex_resourcemanager_folder.myfolder.id
 }
 
-resource "yandex_iam_service_account" "myaccount" {
-  # Создание сервисного аккаунта для взаимодействия с кластером k8s и его компонентами.
-  name        = var.k8s_sa_account
-  description = "K8S zonal service account"
-  folder_id   = yandex_resourcemanager_folder.myfolder.id
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
-  # Сервисному аккаунту назначается роль "k8s.clusters.agent".
-  folder_id = yandex_resourcemanager_folder.myfolder.id
-  role      = "k8s.clusters.agent"
-  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
-  # Сервисному аккаунту назначается роль "vpc.publicAdmin".
-  folder_id = yandex_resourcemanager_folder.myfolder.id
-  role      = "vpc.publicAdmin"
-  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
-  # Сервисному аккаунту назначается роль "container-registry.images.puller".
-  folder_id = yandex_resourcemanager_folder.myfolder.id
-  role      = "container-registry.images.puller"
-  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "encrypterDecrypter" {
-  # Сервисному аккаунту назначается роль "kms.keys.encrypterDecrypter".
-  folder_id = yandex_resourcemanager_folder.myfolder.id
-  role      = "kms.keys.encrypterDecrypter"
-  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "load-balancer-admin" {
-  # Сервисному аккаунту назначается роль "load-balancer.admin".
-  folder_id = yandex_resourcemanager_folder.myfolder.id
-  role      = "load-balancer.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
-}
-
-resource "yandex_kms_symmetric_key" "kms-key" {
-  # Ключ Yandex Key Management Service для шифрования важной информации, такой как пароли, OAuth-токены и SSH-ключи.
-  name              = var.k8s_name_kms_key
-  default_algorithm = "AES_128"
-  rotation_period   = "8760h" # 1 год.
-  folder_id         = yandex_resourcemanager_folder.myfolder.id
-}
-
-resource "yandex_vpc_security_group" "k8s-public-services" {
+resource "yandex_vpc_security_group" "sec-group" {
   # Создание группы безопасности для разграничения сетевого доступа к кластеру и его компонентам".
-  name        = var.k8s_name_sec_group
+  name        = "sec-group"
   description = "Правила группы разрешают подключение к сервисам из интернета."
   network_id  = yandex_vpc_network.mynet.id
   folder_id   = yandex_resourcemanager_folder.myfolder.id
@@ -135,11 +59,157 @@ resource "yandex_vpc_security_group" "k8s-public-services" {
     from_port      = 30000
     to_port        = 32767
   }
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает входящий трафик ssh."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
   egress {
     protocol       = "ANY"
     description    = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
     v4_cidr_blocks = ["0.0.0.0/0"]
     from_port      = 0
     to_port        = 65535
+  }
+}
+
+resource "yandex_compute_instance" "master" {
+  # Создание k8s-master.
+  name                      = "master"
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  allow_stopping_for_update = true
+  folder_id                 = yandex_resourcemanager_folder.myfolder.id
+  resources {
+    cores         = 2
+    memory        = 2 # GB
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8d75k8a0dldkad633n" # Образ the Ubuntu 22.04.
+    }
+  }
+
+  network_interface {
+    nat                = true # Создание внешнего IP ВМ.
+    subnet_id          = yandex_vpc_subnet.mysubnet.id
+    security_group_ids = [yandex_vpc_security_group.sec-group.id]
+  }
+
+  metadata = {
+    user-data = "${file("./cloud-config.yaml")}"
+  }
+
+  labels = {
+    "template-label1" = "master"
+  }
+}
+
+resource "yandex_compute_instance" "node-1" {
+  # Создание первой ноды.
+  name                      = "node-1"
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  allow_stopping_for_update = true
+  folder_id                 = yandex_resourcemanager_folder.myfolder.id
+  resources {
+    cores         = 2
+    memory        = 2 # GB
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8d75k8a0dldkad633n" # Образ the Ubuntu 22.04.
+    }
+  }
+
+  network_interface {
+    nat                = true # Создание внешнего IP ВМ.
+    subnet_id          = yandex_vpc_subnet.mysubnet.id
+    security_group_ids = [yandex_vpc_security_group.sec-group.id]
+  }
+
+  metadata = {
+    user-data = "${file("./cloud-config.yaml")}"
+  }
+
+  labels = {
+    "template-label1" = "node-1"
+  }
+}
+
+resource "yandex_compute_instance" "node-2" {
+  # Создание второй ноды.
+  name                      = "node-2"
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  allow_stopping_for_update = true
+  folder_id                 = yandex_resourcemanager_folder.myfolder.id
+  resources {
+    cores         = 2
+    memory        = 2 # GB
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8d75k8a0dldkad633n" # Образ the Ubuntu 22.04.
+    }
+  }
+
+  network_interface {
+    nat                = true # Создание внешнего IP ВМ.
+    subnet_id          = yandex_vpc_subnet.mysubnet.id
+    security_group_ids = [yandex_vpc_security_group.sec-group.id]
+  }
+
+  metadata = {
+    user-data = "${file("./cloud-config.yaml")}"
+  }
+
+  labels = {
+    "template-label1" = "node-2"
+  }
+}
+
+# Создание ansible inventory
+resource "local_file" "ansible_inventory" {
+  depends_on = [
+    yandex_compute_instance.master,
+    yandex_compute_instance.node-1,
+    yandex_compute_instance.node-2
+  ]
+  content = templatefile("./hosts.ini",
+    {
+      master = yandex_compute_instance.master.network_interface.0.nat_ip_address
+      node-1   = yandex_compute_instance.node-1.network_interface.0.nat_ip_address
+      node-2   = yandex_compute_instance.node-2.network_interface.0.nat_ip_address
+  })
+  filename = "./ansible-install-k8s/inventory.ini"
+}
+
+# Проверка соединения для последующего выполнения плейбука
+resource "terraform_data" "execute-playbook" {
+  provisioner "remote-exec" {
+
+    connection {
+      type        = "ssh"
+      host        = yandex_compute_instance.master.network_interface.0.nat_ip_address
+      user        = "ilya"
+      agent       = false
+      private_key = file(var.private_key)
+    }
+    inline = ["echo 'connected!'"]
+  }
+  depends_on = [
+    local_file.ansible_inventory
+  ]
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ./ansible-install-k8s/inventory.ini --private-key ${var.private_key} ./ansible-install-k8s/ans-k8s.yaml"
   }
 }
