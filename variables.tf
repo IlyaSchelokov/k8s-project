dec5a1c935e08d1fdeba7c09ec972eb0bf860d02
IlyaSchelokov <shelokov.ilya@yandex.ru>
2024-09-29 22:29:59 +0300

    add new variables

--- variables.tf
+++ variables.tf
@@ -4,16 +4,26 @@ variable "my_oauth_token" {
 variable "private_key" {
 }
 
+variable "folder_name" {
+}
 
+variable "net_name" {
+}
 
+variable "subnet_name" {
+}
 
+variable "sec-group_name" {
+}
 
+variable "master_name" {
+}
 
+variable "node1_name" {
+}
 
+variable "node2_name" {
+}
 
-
-
-
-
-
-
+variable "vm_name" {
+}
