8490564119592e82960f08d0e23fcbc1c6772a67
IlyaSchelokov <91783798+IlyaSchelokov@users.noreply.github.com>
2024-10-02 16:43:53 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "public_ip_k8s-master" {
   value = [yandex_compute_instance.master.network_interface.0.nat_ip_address]
 }
+
+output "internal_ip_k8s-master" {
+  value = [yandex_compute_instance.master.network_interface.0.ip_address]
+}
+
+output "internal_ip_node1" {
+  value = [yandex_compute_instance.node1.network_interface.0.ip_address]
+}
+
+output "internal_ip_node2" {
+  value = [yandex_compute_instance.node2.network_interface.0.ip_address]
+}
+
+output "internal_ip_vm" {
+  value = [yandex_compute_instance.vm.network_interface.0.ip_address]
+}
